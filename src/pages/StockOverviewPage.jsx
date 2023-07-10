d7d462e693f63c58c425c86b663903561e55214d
Ugwuoke Victor <113137578+LeoSage01@users.noreply.github.com>
2023-07-10 02:26:55 +0100

    Update StockOverviewPage.jsx

--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -1,11 +1,12 @@
 import { AutoComplete } from "../components/AutoComplete"
 import { StockList } from "../components/StockList"
-// import trading from "../images/"
 
 export const StockOverviewPage = () => {
   return <div>
     <div className="text-center">
-      {/* <img src={trading} /> */}
+       <img style={{
+        height: "120px"
+      }} src="https://i.pinimg.com/564x/bc/3a/f6/bc3af6a60507e30bc69ce7272f63b22e.jpg" alt="" />
     </div>
     <AutoComplete />
     <StockList />
